5c0aa9f843b16a6d73a03e15aefb0e51b456a1bd
John Vajda <john.vajda@deepgram.com>
2025-11-25 15:14:21 -0700

    fix: resolves issues with String|String [] encoding

--- src/deepgram/listen/v1/client.py
+++ src/deepgram/listen/v1/client.py
@@ -54,8 +54,8 @@ class V1Client:
         endpointing: typing.Optional[str] = None,
         extra: typing.Optional[str] = None,
         interim_results: typing.Optional[str] = None,
-        keyterm: typing.Optional[str] = None,
-        keywords: typing.Optional[str] = None,
+        keyterm: typing.Optional[typing.Union[str, typing.Sequence[str]]] = None,
+        keywords: typing.Optional[typing.Union[str, typing.Sequence[str]]] = None,
         language: typing.Optional[str] = None,
         mip_opt_out: typing.Optional[str] = None,
         model: str,
@@ -64,9 +64,9 @@ class V1Client:
         profanity_filter: typing.Optional[str] = None,
         punctuate: typing.Optional[str] = None,
         redact: typing.Optional[str] = None,
-        replace: typing.Optional[str] = None,
+        replace: typing.Optional[typing.Union[str, typing.Sequence[str]]] = None,
         sample_rate: typing.Optional[str] = None,
-        search: typing.Optional[str] = None,
+        search: typing.Optional[typing.Union[str, typing.Sequence[str]]] = None,
         smart_format: typing.Optional[str] = None,
         tag: typing.Optional[str] = None,
         utterance_end_ms: typing.Optional[str] = None,
@@ -172,9 +172,17 @@ class V1Client:
         if interim_results is not None:
             query_params = query_params.add("interim_results", interim_results)
         if keyterm is not None:
-            query_params = query_params.add("keyterm", keyterm)
+            if isinstance(keyterm, str):
+                query_params = query_params.add("keyterm", keyterm)
+            else:
+                for term in keyterm:
+                    query_params = query_params.add("keyterm", term)
         if keywords is not None:
-            query_params = query_params.add("keywords", keywords)
+            if isinstance(keywords, str):
+                query_params = query_params.add("keywords", keywords)
+            else:
+                for keyword in keywords:
+                    query_params = query_params.add("keywords", keyword)
         if language is not None:
             query_params = query_params.add("language", language)
         if mip_opt_out is not None:
@@ -192,11 +200,19 @@ class V1Client:
         if redact is not None:
             query_params = query_params.add("redact", redact)
         if replace is not None:
-            query_params = query_params.add("replace", replace)
+            if isinstance(replace, str):
+                query_params = query_params.add("replace", replace)
+            else:
+                for replacement in replace:
+                    query_params = query_params.add("replace", replacement)
         if sample_rate is not None:
             query_params = query_params.add("sample_rate", sample_rate)
         if search is not None:
-            query_params = query_params.add("search", search)
+            if isinstance(search, str):
+                query_params = query_params.add("search", search)
+            else:
+                for term in search:
+                    query_params = query_params.add("search", term)
         if smart_format is not None:
             query_params = query_params.add("smart_format", smart_format)
         if tag is not None:
@@ -270,8 +286,8 @@ class AsyncV1Client:
         endpointing: typing.Optional[str] = None,
         extra: typing.Optional[str] = None,
         interim_results: typing.Optional[str] = None,
-        keyterm: typing.Optional[str] = None,
-        keywords: typing.Optional[str] = None,
+        keyterm: typing.Optional[typing.Union[str, typing.Sequence[str]]] = None,
+        keywords: typing.Optional[typing.Union[str, typing.Sequence[str]]] = None,
         language: typing.Optional[str] = None,
         mip_opt_out: typing.Optional[str] = None,
         model: str,
@@ -280,9 +296,9 @@ class AsyncV1Client:
         profanity_filter: typing.Optional[str] = None,
         punctuate: typing.Optional[str] = None,
         redact: typing.Optional[str] = None,
-        replace: typing.Optional[str] = None,
+        replace: typing.Optional[typing.Union[str, typing.Sequence[str]]] = None,
         sample_rate: typing.Optional[str] = None,
-        search: typing.Optional[str] = None,
+        search: typing.Optional[typing.Union[str, typing.Sequence[str]]] = None,
         smart_format: typing.Optional[str] = None,
         tag: typing.Optional[str] = None,
         utterance_end_ms: typing.Optional[str] = None,
@@ -388,9 +404,17 @@ class AsyncV1Client:
         if interim_results is not None:
             query_params = query_params.add("interim_results", interim_results)
         if keyterm is not None:
-            query_params = query_params.add("keyterm", keyterm)
+            if isinstance(keyterm, str):
+                query_params = query_params.add("keyterm", keyterm)
+            else:
+                for term in keyterm:
+                    query_params = query_params.add("keyterm", term)
         if keywords is not None:
-            query_params = query_params.add("keywords", keywords)
+            if isinstance(keywords, str):
+                query_params = query_params.add("keywords", keywords)
+            else:
+                for keyword in keywords:
+                    query_params = query_params.add("keywords", keyword)
         if language is not None:
             query_params = query_params.add("language", language)
         if mip_opt_out is not None:
@@ -408,11 +432,19 @@ class AsyncV1Client:
         if redact is not None:
             query_params = query_params.add("redact", redact)
         if replace is not None:
-            query_params = query_params.add("replace", replace)
+            if isinstance(replace, str):
+                query_params = query_params.add("replace", replace)
+            else:
+                for replacement in replace:
+                    query_params = query_params.add("replace", replacement)
         if sample_rate is not None:
             query_params = query_params.add("sample_rate", sample_rate)
         if search is not None:
-            query_params = query_params.add("search", search)
+            if isinstance(search, str):
+                query_params = query_params.add("search", search)
+            else:
+                for term in search:
+                    query_params = query_params.add("search", term)
         if smart_format is not None:
             query_params = query_params.add("smart_format", smart_format)
         if tag is not None:
